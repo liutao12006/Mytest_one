7dfbbb719df29b7ac9c61f38f3694da8c86f7f7e
liutao12006 <360215039@qq.com>
2016-11-03 17:03:15 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/mytest/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.administrator.mytest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.print("我们的歌");
+        Button debug = (Button) findViewById(R.id.button3);
+        debug.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"womwndege",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        show();
+    }
+
+    private void show(){
+
+    }
+//    private void onClick(View view){
+//        switch (view.getId()) {
+//            case R.id.button3:
+//                Toast.makeText(this,"womwndege",Toast.LENGTH_LONG).show();
+//                break;
+//        }
+//    }
+
+}
